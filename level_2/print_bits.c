0bdae017ec12e7819fe17fa2480c2ef39e266e36
Hugo Ganet <ganethugo@gmail.com>
2025-01-30 12:21:49 +0100

    level_2 print_bits

---
+++ level_2/print_bits.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hganet <hganet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 15:45:12 by hganet            #+#    #+#             */
+/*   Updated: 2025/01/30 12:21:33 by hganet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int	i;
+	unsigned char	bit;
+
+	i = 8;
+	bit = 0;
+	while (i--)
+	{
+		bit = (octet >> i & 1) + 48;
+		write (1, &bit, 1);
+	}
+}
+
+int main()
+{
+	unsigned char octet = 3;
+	print_bits(octet);
+}
